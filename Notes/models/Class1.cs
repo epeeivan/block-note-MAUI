84a02f6bcb33330b669f993e768c771b8f1a2a89
epeeivan <epeeivan@gmail.com>
2022-10-13 17:02:23 +0100

    Create Class1.cs

---
+++ Notes/models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.models
+{
+    public class Note
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+        public string date { get; set; }
+    }
+}
